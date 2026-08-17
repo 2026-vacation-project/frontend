505f4670d8d02876bf4ca2369db155b06ce57c7d
Pro203S <prosirea2501@gmail.com>
2026-08-18 05:03:04 +0900

    프로젝트 세팅

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router';
+import './index.css';
+
+import { default as IndexPage } from './App';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+        <Routes>
+            <Route index element={<IndexPage />} />
+        </Routes>
+    </BrowserRouter>,
 )
